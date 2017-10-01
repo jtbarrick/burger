132a55e3ad6def68ed72616efde8baf9dcbeac7b
jtbarrick <jeffrey.barrick@gmail.com>
2017-09-30 22:53:24 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Classic American", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bleu Cheeseburger", false);
